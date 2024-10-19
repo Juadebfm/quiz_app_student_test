1b861e51b2d9f24cfea14ea26c36744924368982
Julius Adebowale <juadebgabriel@gmail.com>
2024-10-19 21:02:35 +0100

    fix

--- readme.md
+++
@@ -1,5 +0,0 @@
-# QUIZ APP
-
-## LINKS
-- ### API DOC: https://documenter.getpostman.com/view/23324874/2sAXxV5Uxu
-- ### API URL - https://quiz-snowy-psi.vercel.app/api
--- utils/responseFormatter.js
+++
@@ -1,7 +0,0 @@
-export const formatResponse = (success, message, data = null) => {
-  const response = { success, message };
-  if (data) {
-    response.data = data;
-  }
-  return response;
-};
